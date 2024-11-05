df2e6b60bb1329b4bc31da1039b36f0ae5d2d87e
mafiapatch <g8071107@gmail.com>
2024-11-05 21:39:49 +0800

    [modify] footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,37 +1,59 @@
 <template>
-    <footer class="footer">
+  <footer class="footer">
+    <div class="container">
+
+      <div class="row">
+        <div class="col-12 col-lg-5 company_info d-none d-lg-block">
+          <img class="logo" src="/assets/img/footer-logo.png" />
+          <h3>孚瑞科技有限公司</h3>
+          <p>114 台北市內湖區民權東路六段180巷6號11樓之23</p>
+        </div>
+        <div class="col-12 col-lg-3 contact d-none d-lg-flex">
+          <p>
+            Tel : +886-2-22173688<br />
+            Fax : +886-2-22176411<br />
+            service@fluorez.com.tw
+          </p>
+        </div>
+        <div class="col-12 col-lg-4 slogan d-none d-lg-block">
+          <h4>
+            The<span> solution provider </span>of<br />
+            perfluoroelastome.
+          </h4>
+          <span class="copyright"> Fluorez Technology Inc.© 2024</span>
+        </div>
+
+        <div class="col-6 company_info d-block d-lg-none">
+          <img class="logo" src="/assets/img/footer-logo.png" />
+          <h3>孚瑞科技有限公司</h3>
+        </div>
+
+        <div class="col-6 slogan d-block d-lg-none">
+          <h4>
+            The<span> solution provider </span>of<br />
+            perfluoroelastome.
+          </h4>
+        </div>
+      </div>
+    </div>
+    <div id="footer-point">
         <div class="row">
-                <div class="col-2">
-                    <img class="left-point" src="/assets/img/footer_point.png">
-                </div>
-
-                <div class="col-3 company_info">
-                    <img class="logo" src="/assets/img/footer-logo.png">
-                    <h3>孚瑞科技有限公司</h3>
-                    <p>114 台北市內湖區民權東路六段180巷6號11樓之23</p>
-                </div>
-                <div class="col-2 contact">
-                    <p>Tel : +886-2-22173688<br/>
-                    Fax : +886-2-22176411<br/>
-                    service@fluorez.com.tw</p>
-                </div>
-                <div class="col-3 slogan">
-                    <h4>The<span> solution provider </span>of<br>
-                        perfluoroelastome.</h4>
-                    <span class="copyright"> Fluorez Technology Inc.© 2024</span>
-                </div>
-
-                <div class="col-2">
-                    <img class="right-point" src="/assets/img/footer_point.png">
-                </div>
+            <div class="col-2">
+                <img class="left-point" src="/assets/img/footer_point.png">
             </div>
-    </footer>
+            <div class="col-8">
+            </div>
+            <div class="col-2">
+                <img class="right-point" src="/assets/img/footer_point.png">
+            </div>         
+        </div>
+    </div>
+  </footer>
 </template>
 
 <script>
 export default {
-    name: 'Footer',
-    setup() {
-    }
-}
+  name: "Footer",
+  setup() {},
+};
 </script>
