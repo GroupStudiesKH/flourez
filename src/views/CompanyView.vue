3802060dd68f69faaa638b73e2908c15356e7a59
mafiapatch <g8071107@gmail.com>
2024-11-10 10:15:58 +0800

    [modify] progress backup

--- src/views/CompanyView.vue
+++ src/views/CompanyView.vue
@@ -81,6 +81,26 @@ export default {
           </div>
         </div>
       </div>
+
+      <div class="container text-section">
+        <h4>專業</h4>
+        <p>孚瑞科技有限公司的專業來自於多年來在市場上累積的經驗與口碑。我們相信良好的溝通才是選擇正確材料的不二法則，孚瑞科技不僅提供多元化之產品線，包含全氟化彈性體的混練膠，我們也同時提供氟化彈性體(fluoro-elastomer, FKM)與全氟化橡膠(perfluoro-elastomers, FFKM)製造之標準部元件；同時亦接受針對消費者需求來量身訂製的客製化FKM/FFKM產品。我們不僅不斷開發符合應用端的新配方，也開發了符合FDA與USP Class VI的產品以滿足食品與醫療產業的需求。</p>
+      </div>
+
+      <div class="container text-section">
+        <h4>品質</h4>
+        <p>孚瑞科技有限公司符合ISO品管認證，產品皆經過第三方實驗室檢驗，符合歐盟最新化學品管制與有害化學品等一系列標準。無塵室內生產的產品規範也遵循國際標準ASTM與ISO的規範。最高標準的產品抽樣檢測也成為支持我們提供高標準產品的後盾。</p>
+      </div>
+
+      <div class="container text-section">
+        <h4>服務</h4>
+        <p>多年來我們與不同客戶合作各式OEM與ODM的生產模式，不只步於生產銷售，而是提供一個全整的解決方案給客戶。我們也提供銷售前與售後的實驗室分析，支援客戶的問題解決方案。客製化的庫存管理方案更能滿足客戶在效率與成本上的追求。</p>
+      </div>
+
+
+      <div id="company_center_image">
+      </div>
+
     </div>
   </main>
   <Footer />
