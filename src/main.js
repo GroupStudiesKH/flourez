b7cee758ceecb5d833af2e7cf298093e1357ef27
mafiapatch <g8071107@gmail.com>
2024-11-11 00:01:52 +0800

    [modify] update css

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 import "./assets/main.scss";
-import 'bootstrap/dist/css/bootstrap.min.css'
+// import 'bootstrap/dist/css/bootstrap.min.css'
 import "bootstrap"
 import { createApp } from "vue";
 import App from "./App.vue";
