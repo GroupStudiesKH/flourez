5452339dd0dc68824e1dc0d8d8ee7ad5386b5aa7
mafiapatch <g8071107@gmail.com>
2024-12-12 13:52:09 +0800

    [modify] add ga

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -4,13 +4,13 @@
       <div class="row">
         <div class="col-12 col-lg-5 company_info d-none d-lg-block">
           <img class="logo" src="/assets/img/footer-logo.png" />
-          <h3>{{ t('footer.company') }}</h3>
-          <p>{{ t('footer.address') }}</p>
+          <h3>{{ t("footer.company") }}</h3>
+          <p>{{ t("footer.address") }}</p>
         </div>
         <div class="col-12 col-lg-3 contact d-none d-lg-flex">
           <p>
-            Tel : {{ t('footer.phone') }}<br />
-            Fax : {{ t('footer.fax') }}<br />
+            Tel : {{ t("footer.phone") }}<br />
+            Fax : {{ t("footer.fax") }}<br />
             service@fluorez.com.tw
           </p>
         </div>
@@ -19,43 +19,46 @@
             The<span> solution provider </span>of<br />
             perfluoroelastome.
           </h4>
-          <span class="copyright"> {{ t('footer.intro') }}© 2024</span>
+          <span class="copyright"> {{ t("footer.intro") }}© 2024</span>
         </div>
 
         <div class="col-6 company_info d-block d-lg-none">
           <img class="logo" src="/assets/img/footer-logo.png" />
-          <h3>{{ t('footer.company') }}</h3>
+          <h3>{{ t("footer.company") }}</h3>
         </div>
 
         <div class="col-6 slogan d-block d-lg-none">
           <h4>
-            The<br/><span> solution provider </span>of<br />
+            The<br /><span> solution provider </span>of<br />
             perfluoroelastome.
           </h4>
         </div>
         <div class="col-12 company_info d-block d-lg-none">
-          <p class="bottom_company_info">{{ t('footer.address') }} / {{ t('footer.phone') }}</p>
+          <p class="bottom_company_info">
+            {{ t("footer.address") }} / {{ t("footer.phone") }}
+          </p>
         </div>
       </div>
     </div>
     <div id="footer-point">
       <div class="row">
         <div class="col-2">
-          <img class="left-point" src="/assets/img/footer_point.png">
+          <img class="left-point" src="/assets/img/footer_point.png" />
         </div>
-        <div class="col-8">
-        </div>
+        <div class="col-8"></div>
         <div class="col-2">
-          <img class="right-point" src="/assets/img/footer_point.png">
-        </div>         
+          <img class="right-point" src="/assets/img/footer_point.png" />
+        </div>
       </div>
     </div>
 
     <!-- Cookie 同意提示框 -->
     <div v-if="!cookieAccepted" class="cookie-consent">
       <div class="cookie-content">
-        <p>{{ t('cookie.consent') }}</p>
-        <button @click="acceptCookies" class="accept-btn">{{ t('cookie.accept') }}</button>
+        <p>{{ t("cookie.consent") }}</p>
+        <button @click="acceptCookies" class="accept-btn">
+          {{ t("cookie.accept") }}
+        </button>
       </div>
     </div>
   </footer>
@@ -67,25 +70,38 @@ import { useI18n } from "vue-i18n";
 export default {
   name: "Footer",
   data() {
-
     const { t, locale } = useI18n();
 
     return {
       cookieAccepted: false,
       t,
       locale,
-    }
+    };
   },
   mounted() {
     // 檢查是否已經同意過 cookies
-    this.cookieAccepted = localStorage.getItem('cookieAccepted') === 'true'
+    this.cookieAccepted = localStorage.getItem("cookieAccepted") === "true";
+
+    if (this.cookieAccepted) {
+      gtag("consent", "update", {
+        ad_storage: "granted",
+        analytics_storage: "granted",
+      });
+      console.log("cookieAccepted", this.cookieAccepted);
+    } else {
+      gtag("consent", "update", {
+        ad_storage: "denied",
+        analytics_storage: "denied",
+      });
+      console.log("cookieAccepted", this.cookieAccepted);
+    }
   },
   methods: {
     acceptCookies() {
-      this.cookieAccepted = true
-      localStorage.setItem('cookieAccepted', 'true')
-    }
-  }
+      this.cookieAccepted = true;
+      localStorage.setItem("cookieAccepted", "true");
+    },
+  },
 };
 </script>
 
@@ -110,7 +126,7 @@ export default {
 }
 
 .accept-btn {
-  background: #BE1E2D;
+  background: #be1e2d;
   color: white;
   border: none;
   padding: 0.5rem 1.5rem;
@@ -128,7 +144,7 @@ export default {
     flex-direction: column;
     text-align: center;
   }
-  
+
   .accept-btn {
     margin-top: 1rem;
     margin-left: 0;
