34f1aa3c3c242aee1d4eeb2a9da78f9893431c85
mafiapatch <g8071107@gmail.com>
2025-04-03 07:25:42 +0800

    feat(ProductsMenu): 根据locale动态渲染产品菜单
    添加条件渲染逻辑，根据locale的值显示不同的产品菜单内容。当locale为'eng'时显示原有菜单，否则显示新的本地化菜单

--- src/components/ProductsMenu.vue
+++ src/components/ProductsMenu.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="container" id="badge_area">
+    <div class="container" id="badge_area" v-if="locale == 'eng'">
         <div class="row">
             <div class="col-6 col-lg-3">
                 <a href="ffkm">
@@ -20,6 +20,43 @@
                 <a href="pfls">
                     <img src="/assets/img/m_product_icon04.png" alt="" class="img-fluid" />
                 </a>
+            </div>
+        </div>
+    </div>
+
+    <div class="container" id="badge_area" v-else>
+        <div class="row">
+            <div class="col-6 col-md-3 badge">
+            <a href="ffkm">
+                <div class="outline">
+                    <img src="/assets/img/ffkm.png" alt="" />
+                    <h4>{{ t('product.ffkm.title') }}</h4>
+                </div>
+            </a>
+            </div>
+            <div class="col-6 col-md-3 badge">
+            <a href="fkm">
+                <div class="outline" style="margin: 0 10px 0 0">
+                    <img src="/assets/img/fkm.png" alt="" />
+                    <h4>{{ t('product.fkm.title') }}</h4>
+                </div>
+            </a>
+            </div>
+            <div class="col-6 col-md-3 badge">
+            <a href="specfkm">
+                <div class="outline" style="margin: 0 10px 0 0">
+                    <img src="/assets/img/spec_fkm.png" alt="" />
+                    <h4>{{ t('product.specFkm.title') }}</h4>
+                </div>
+            </a>
+            </div>
+            <div class="col-6 col-md-3 badge">
+            <a href="pfls">
+                <div class="outline" style="margin: 0 0 0 5px">
+                    <img src="/assets/img/pfls.png" alt="" />
+                    <h4>{{ t('product.pfls.title') }}</h4>
+                </div>
+            </a>
             </div>
         </div>
     </div>
